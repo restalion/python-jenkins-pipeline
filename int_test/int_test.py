c4554d5da391a23e8fa946569f1623203121763b
Julio Palma Vázquez <julio.palma.vazquez@MacBook-Pro-de-Julio.local>
2018-10-19 13:12:54 +0200

    add logs

--- int_test/int_test.py
+++ int_test/int_test.py
@@ -7,5 +7,6 @@ class IntegrationTest(unittest.TestCase):
 
     def test_function(self):
         tester = app.test_client(self)
+        print(tester.environ_base)
         response = tester.get('/auth/hello', content_type='html/text')
         self.assertEqual(response.status_code, 200)
