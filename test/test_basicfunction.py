a426741e8fa9ecfd012da784c31ab97dcc8c1d31
Julio Palma Vázquez <julio.palma.vazquez@MacBook-Pro-de-Julio.local>
2018-10-19 13:34:30 +0200

    solve static code errors

--- test/test_basicfunction.py
+++ test/test_basicfunction.py
@@ -1,28 +1,29 @@
 import unittest
 from app.basicfunction import BasicFunction
- 
+
+
 class TestBasicFunction(unittest.TestCase):
     def setUp(self):
         self.func = BasicFunction()
- 
+
     def test_1(self):
         self.assertTrue(True)
- 
+
     def test_2(self):
         self.assertTrue(True)
- 
+
     def test_3(self):
         self.assertEqual(self.func.state, 0)
- 
+
     def test_4(self):
         self.func.increment_state()
         self.assertEqual(self.func.state, 1)
- 
+
     def test_5(self):
         self.func.increment_state()
         self.func.increment_state()
         self.func.clear_state()
         self.assertEqual(self.func.state, 0)
- 
+
 if __name__ == '__main__':
     unittest.main()
